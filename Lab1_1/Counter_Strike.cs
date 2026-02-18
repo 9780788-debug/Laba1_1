7a5b6b064903b8fb413a9df1672bdb821a0a37e6
borsc <borsc@FLUFFYX>
2026-02-18 13:05:53 -0800

    Add project files.

---
+++ Lab1_1/Counter_Strike.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab1_1
+{
+    class Counter_Strike
+    {
+         private double x1, y1, x2, y2;
+         
+         public Counter_Strike(double x1, double y1, double x2, double y2) 
+         {
+             this.x1 = x1;
+             this.y1 = y1;
+             this.x2 = x2;
+             this.y2 = y2;
+         }
+         public double X1 => x1;
+         public double Y1 => y1;
+         public double X2 => x2;
+         public double Y2 => y2;
+
+        public double GetLength() 
+        {
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+
+            double length = Math.Sqrt(dx * dx + dy * dy);
+            return length;
+        }
+
+        public double GetAngleWithOY()
+        {
+            double length = GetLength();
+            if (length == 0)
+                return 0;
+
+            double dy = y2 - y1;
+            double angle = Math.Acos(dy / length);
+            double angle_in_degrees = angle * (180.0 / Math.PI);
+            return angle_in_degrees;
+        }
+
+
+    }
+}
---
+++ Lab1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter_Strike source = new Counter_Strike(0, 0, 3, 4);
+            
+            Console.WriteLine("Start coo: 5(" + source.X1 + ", "+ source.Y1 + ")");
+            Console.WriteLine("End coo: (" + source.X2 + ", "+ source.Y2 + ")");
+            Console.WriteLine("Length is for real: " + source.GetLength());
+        }
+
+    }
+}
